e2a80de89093892ab6d48c045a7859f38ddbe802
DESKTOP-E1UH8SQ\Rizal Zulkipli <02rizalzulkipli@gmail.com>
2023-10-24 01:10:53 +0700

    Before lsp

---
+++ lsp/Smartphone.java
@@ -0,0 +1,11 @@
+class Smartphone extends Product {
+    @Override
+    String setName() {
+        return "Samsung S10+ Limited Edition";
+    }
+
+    @Override
+    Date setExpiredDate() {
+        return new Date(); // ???????
+    }
+}
---
+++ lsp/Vegetable.java
@@ -0,0 +1,11 @@
+class Vegetable extends Product {
+    @Override
+    String getName() {
+        return "Broccoli";
+    }
+
+    @Override
+    Date getExpiredDate() {
+        return new Date();
+    }
+}
