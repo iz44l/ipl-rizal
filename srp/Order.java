e8a763543357de49d8cd918e1b602d768bcc1056
DESKTOP-E1UH8SQ\Rizal Zulkipli <02rizalzulkipli@gmail.com>
2023-10-24 01:00:39 +0700

    After srp

--- srp/Order.java
+++ srp/Order.java
@@ -1,35 +1,16 @@
 class Order {
     void calculateTotalSum() {
-        /* ... */
     }
 
     void getItems() {
-        /* ... */
     }
 
     void getItemCount() {
-        /* ... */
     }
 
     void addItem(Item item) {
-        /* ... */
     }
 
     void deleteItem(Item item) {
-        /* ... */
-    }
-
-    void printOrder() {
-        /* ... */
-    }
-
-    void showOrder() {
-        /* ... */
-    }
-
-    void getDailyHistory() {
-    }
-
-    void getMonthlyHistory() {
     }
 }
